5711f730772ea4a0662c7ff26461252f16f4a8ec
Alexander-Eck <alexander.eck@hwr-berlin.de>
2026-04-22 15:36:49 +0200

    Add 'sqlalchemy' content

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-import os
 from flask import Flask, render_template, redirect, url_for, request, abort, flash
 from flask_bootstrap import Bootstrap5
-import db, forms
+import forms
 
 app = Flask(__name__)
 
 app.config.from_mapping(
-    SECRET_KEY='secret_key_just_for_dev_environment',
-    DATABASE=os.path.join(app.instance_path, 'todos.sqlite'),
+    SECRET_KEY = 'secret_key_just_for_dev_environment',
     BOOTSTRAP_BOOTSWATCH_THEME = 'pulse'
 )
-app.cli.add_command(db.init_db)
-app.teardown_appcontext(db.close_db_con)
+
+from db import db, Todo, List, insert_sample  # (1.)
 
 bootstrap = Bootstrap5(app)
 
@@ -22,64 +20,46 @@ def index():
 
 @app.route('/todos/', methods=['GET', 'POST'])
 def todos():
-    db_con = db.get_db_con()
     form = forms.CreateTodoForm()
     if request.method == 'GET':
-        sql_query = 'SELECT * FROM todo ORDER BY id;'
-        todos = db_con.execute(sql_query).fetchall()
+        todos = db.session.execute(db.select(Todo).order_by(Todo.id)).scalars()  # !!
         return render_template('todos.html', todos=todos, form=form)
     else:  # request.method == 'POST'
         if form.validate():
-            sql_query = 'INSERT INTO todo (description) VALUES (?);'
-            db_con.execute(sql_query, [form.description.data])
-            db_con.commit()
+            todo = Todo(description=form.description.data)  # !!
+            db.session.add(todo)  # !!
+            db.session.commit()  # !!
             flash('Todo has been created.', 'success')
-            print('Todo has been created.')
         else:
             flash('No todo creation: validation error.', 'warning')
-            print('No todo creation: validation error.')
         return redirect(url_for('todos'))
 
 @app.route('/todos/<int:id>', methods=['GET', 'POST'])
 def todo(id):
-    db_con = db.get_db_con()
+    todo = db.session.get(Todo, id)  # !!
+    form = forms.TodoForm(obj=todo)  # (2.)  # !!
     if request.method == 'GET':
-        sql_query = (
-            'SELECT * FROM '
-            '( SELECT * FROM todo '
-                'LEFT JOIN todo_list '
-                'ON todo.id=? AND todo_id=todo.id ) '
-            'WHERE id=?;'
-        )
-        todo = db_con.execute(sql_query, [id, id]).fetchone()
         if todo:
-            form = forms.TodoForm(data=todo)
-            sql_query = 'SELECT id, name FROM list ORDER BY name;'
-            choices = db_con.execute(sql_query).fetchall()
-            form.list_id.choices = [(0, 'List?')] + [(c['id'], c['name']) for c in choices]
+            if todo.lists: form.list_id.data = todo.lists[0].id  # (3.)  # !!
+            choices = db.session.execute(db.select(List).order_by(List.name)).scalars()  # !!
+            form.list_id.choices = [(0, 'List?')] + [(c.id, c.name) for c in choices]  # !!
             return render_template('todo.html', form=form)
         else:
             abort(404)
-    else: # request.method == 'POST'
-        form = forms.TodoForm()
+    else:  # request.method == 'POST'
         if form.method.data == 'PATCH':
             if form.validate():
-                sql_query = 'UPDATE todo SET complete = ?, description = ? WHERE id = ?;'
-                db_con.execute(sql_query, [form.complete.data, form.description.data, id])
-                sql_query = 'DELETE FROM todo_list WHERE todo_id = ?;'
-                db_con.execute(sql_query, [id])
-                if form.list_id.data:
-                    sql_query = 'INSERT INTO todo_list (todo_id, list_id) VALUES (?, ?);'
-                    db_con.execute(sql_query, [id, form.list_id.data])
-                db_con.commit()
+                form.populate_obj(todo)  # (4.)
+                todo.populate_lists([form.list_id.data])  # (5.)  # !!
+                db.session.add(todo)  # !!
+                db.session.commit()  # !!
                 flash('Todo has been updated.', 'success')
             else:
                 flash('No todo update: validation error.', 'warning')
             return redirect(url_for('todo', id=id))
         elif form.method.data == 'DELETE':
-            sql_query = 'DELETE FROM todo WHERE id = ?;'
-            db_con.execute(sql_query, [id])
-            db_con.commit()
+            db.session.delete(todo)  # !!
+            db.session.commit()  # !!
             flash('Todo has been deleted.', 'success')
             return redirect(url_for('todos'), 303)
         else:
@@ -88,51 +68,20 @@ def todo(id):
 
 @app.route('/lists/')
 def lists():
-    db_con = db.get_db_con()
-    sql_query = 'SELECT * from list ORDER BY name;'
-    lists_temp = db_con.execute(sql_query).fetchall()
-    lists = []
-    for list_temp in lists_temp:
-        list = dict(list_temp)
-        sql_query = (
-            'SELECT COUNT(complete) = SUM(complete) '
-            'AS complete FROM todo '
-            f'JOIN todo_list ON list_id={list["id"]} '
-                'AND todo_id=todo.id; '
-        )
-        complete = db_con.execute(sql_query).fetchone()['complete']
-        list['complete'] = complete
-        lists.append(list)
-    if request.args.get('json') is not None:
-        return lists
-    else:
-        return render_template('lists.html', lists=lists)
+    lists = db.session.execute(db.select(List).order_by(List.name)).scalars()  # (6.)  # !!
+    return render_template('lists.html', lists=lists)
 
 @app.route('/lists/<int:id>')
 def list(id):
-    db_con = db.get_db_con()
-    sql_query_1 = f'SELECT name FROM list WHERE id={id}'
-    sql_query_2 = (
-        'SELECT id, complete, description FROM todo '
-        f'JOIN todo_list ON todo_id=todo.id AND list_id={id} '
-        'ORDER BY id;'
-    )
-    list = {}
-    result = db_con.execute(sql_query_1).fetchone()
-    if result is not None:
-        list['name'] = result['name']
-        list['todos'] = db_con.execute(sql_query_2).fetchall()
-        if request.args.get('json') is not None:
-            list['todos'] = [dict(todo) for todo in list['todos']]
-            return list
-        else:
-            return render_template('list.html', list=list)
+    list = db.session.get(List, id)  # !!
+    if list is not None:
+        return render_template('list.html', list=list)
     else:
         return redirect(url_for('lists'))
 
 @app.route('/insert/sample')
 def run_insert_sample():
-    db.insert_sample()
+    insert_sample()
     return 'Database flushed and populated with some sample data.'
 
 @app.errorhandler(404)
@@ -143,7 +92,7 @@ def http_not_found(e):
 def http_internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.get('/faq/<css>') # /faq/alt: alternative colors; /faq/none: no css applied
+@app.get('/faq/<css>')
 @app.get('/faq/', defaults={'css': 'default'})
 def faq(css):
     return render_template('faq.html', css=css)
--- db.py
+++ db.py
@@ -1,38 +1,79 @@
 import click
-import os
-import sqlite3
-from flask import current_app, g
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import orm
+from app import app
 
-def get_db_con(pragma_foreign_keys = True):
-    if 'db_con' not in g:
-        g.db_con = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db_con.row_factory = sqlite3.Row
-        if pragma_foreign_keys:
-            g.db_con.execute('PRAGMA foreign_keys = ON;')
-    return g.db_con
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.sqlite'
 
-def close_db_con(e=None):
-    db_con = g.pop('db_con', None)
-    if db_con is not None:
-        db_con.close()
+db = SQLAlchemy()
+db.init_app(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    complete = db.Column(db.Boolean, default=False)
+    description = db.Column(db.String, nullable=False)
+    lists = db.relationship('List', secondary='todo_list', back_populates='todos')
+
+    def populate_lists(self, list_ids):
+        lists = []
+        for id in list_ids:
+            if id > 0: lists.append(db.session.get(List, id))
+        self.lists = lists
+
+class List(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    name = db.Column(db.String, nullable=False)
+    todos = db.relationship(Todo, secondary='todo_list', back_populates='lists')
+    complete = False
+    
+    @orm.reconstructor
+    def check_complete(self):
+        self.complete = all([todo.complete for todo in self.todos])
+
+todo_list = db.Table(
+    'todo_list',
+    db.Column('todo_id', db.Integer, db.ForeignKey('todo.id'), primary_key=True),
+    db.Column('list_id', db.Integer, db.ForeignKey('list.id'), primary_key=True)
+)
+
+with app.app_context():
+    db.create_all()
 
 @click.command('init-db')
-def init_db():
-    try:
-        os.makedirs(current_app.instance_path)
-    except OSError:
-        pass
-    db_con = get_db_con()
-    with current_app.open_resource('sql/drop_tables.sql') as f:
-        db_con.executescript(f.read().decode('utf8'))
-    with current_app.open_resource('sql/create_tables.sql') as f:
-        db_con.executescript(f.read().decode('utf8'))
+def init():
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
     click.echo('Database has been initialized.')
 
+app.cli.add_command(init) 
+
 def insert_sample():
-    db_con = get_db_con()
-    with current_app.open_resource('sql/insert_sample.sql') as f:
-        db_con.executescript(f.read().decode('utf8'))
+    # Delete all existing data, if any
+    db.session.execute(db.delete(todo_list))
+    db.session.execute(db.delete(Todo))
+    db.session.execute(db.delete(List)) 
+
+    # Create sample to-do items
+    todo1 = Todo(complete=True, description='Get some food')
+    todo2 = Todo(description='Drive the bike more often')
+    todo3 = Todo(description='Implement web app')
+    todo4 = Todo(complete=True, description='Call mom')
+    todo5 = Todo(complete=True, description='Clean up') 
+
+    # Create sample to-do list items
+    list1 = List(name='Life')
+    list2 = List(name='Work')
+    list3 = List(name='Family')
+
+    # Associate to-do items to lists by filling their `Todo.list` attribute
+    todo1.lists.append(list1)
+    todo2.lists.append(list1)
+    todo2.lists.append(list2)
+    todo3.lists.append(list2)
+    todo4.lists.append(list3)
+    todo5.lists.append(list3)   
+
+    # Add all objects to the queue and commit them to the database
+    db.session.add_all([todo1, todo2, todo3, todo4, todo5, list1, list2, list3])
+    db.session.commit()
