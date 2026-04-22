e4b19dacd6bf9f8972d76f2559348f1a54b6dade
Alexander-Eck <alexander.eck@hwr-berlin.de>
2026-04-22 16:04:50 +0200

    Delete SECRET_KEY config

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import db
 app = Flask(__name__)
 
 app.config.from_mapping(
-    SECRET_KEY='secret_key_just_for_dev_environment',
     DATABASE=os.path.join(app.instance_path, 'todos.sqlite')
 )
 app.cli.add_command(db.init_db)
